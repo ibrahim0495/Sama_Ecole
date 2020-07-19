8c88058cd9af54d21fc27b5e70b22e1eea7cf0ab
Ouzy012 <ousmane.ndiaye8518@gmail.com>
2020-07-19 23:03:45 +0000

    header page

--- resources/views/pages/directeur/header.blade.php
+++ resources/views/pages/directeur/header.blade.php
@@ -17,7 +17,6 @@
                     <a href="#" class="btn btn-sm btn-neutral">Filters</a>
                 </div>
             </div>
-<<<<<<< HEAD
             <!-- Card stats -->
             <div class="row">
                 <div class="col-xl-3 col-md-6">
@@ -109,10 +108,6 @@
                     </div>
                 </div>
             </div>
-=======
-
-            @yield('header')
->>>>>>> create formulaire
         </div>
     </div>
 </div>
