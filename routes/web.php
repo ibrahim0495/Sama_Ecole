3968a4b00e8dc389fad13d01dea26bc0795fbe76
Ouzy012 <ousmane.ndiaye8518@gmail.com>
2020-07-19 22:53:45 +0000

    correct bug from git hub

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,3 @@ Route::get('/dashboard', function () {
     return view('pages.directeur.home');
 });
 
-// Authentification
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-// Routes directeur
-Route::get('/etabilissement', function () {
-    return view('pages.directeur.ajouterEtablissement');
-});
