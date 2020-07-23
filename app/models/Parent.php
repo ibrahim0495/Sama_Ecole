8507765dd1b08004a4f7ee99cd1b476a5106cc0a
MoussaJoe <moussadiegane.sarr@uadb.edu.sn>
2020-07-23 15:31:12 +0000

    le modele

---
+++ app/models/Parent.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+#la classe est nommee Paarent parce que Parent est une classe reservee
+class Paarent extends Model  
+{
+    //
+}
